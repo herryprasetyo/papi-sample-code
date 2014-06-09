e36b012b7a9ed127771fe6a13132ff528d452b1f
shaddiqa <radityo.shaddiqa@veritrans.co.id>
2014-06-09 14:28:14 +0700

    commit config.php

---
+++ VT-Web/PHP/config.php
@@ -0,0 +1,14 @@
+<?php
+class Config
+{
+	// TODO: change with your actual server_key that can be found on Merchant Administration Portal (MAP)
+	const SERVER_KEY = "62b15a7d-e5f8-4b3c-9eab-ca827ca37f55";
+
+
+	// TODO : change to production URL for your production Environment
+	// sandbox/development/testing environment:
+	const ENDPOINT = "https://api.sandbox.veritrans.co.id/v2/charge";
+	// production environment:
+	//const ENDPOINT = "https://api.sandbox.veritrans.co.id/v2/charge";
+}
+?>
